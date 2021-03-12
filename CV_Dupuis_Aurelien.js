b4f649266287f062370e4fda5babc74d62e01295
Aurélien <dupuis.aurelien.eps@outlook.fr>
2021-03-12 15:34:13 +0100

    themes

--- CV_Dupuis_Aurelien.js
+++ CV_Dupuis_Aurelien.js
@@ -25,3 +25,35 @@ window.addEventListener("scroll", function() {
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
+
+/* Changement théme couleurs */
+
+/* Vert */
+
+const btn1 = document.querySelector('.theme-vert')
+
+btn1.addEventListener('click', () => {
+
+    document.documentElement.style.setProperty('--main-color', '#023618');
+    document.documentElement.style.setProperty('--shade-color', '#9aec7f');
+})
+
+/* Bleu */
+
+const btn2 = document.querySelector('.theme-bleu')
+
+btn2.addEventListener('click', () => {
+
+    document.documentElement.style.setProperty('--main-color', '#222f51');
+    document.documentElement.style.setProperty('--shade-color', '#03BFCB');
+})
+
+/* Rouge */
+
+const btn3 = document.querySelector('.theme-rouge')
+
+btn3.addEventListener('click', () => {
+
+    document.documentElement.style.setProperty('--main-color', '#4c0808');
+    document.documentElement.style.setProperty('--shade-color', '#ff6b60');
+})
